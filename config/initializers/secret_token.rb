18681d7acbbe3235bc8feab8ad61eddfde476cf5
Daniel Borsinger <daniel.borsinger@gmail.com>
2011-11-08 19:55:44 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyRatings::Application.config.secret_token = '0b089b16e5476b9198feb48dbbeeddb5668ebb2bdf8f478cb618b9ef59ba589ee7f45f0117869e57af9f877ba46b716cf3e1feab2b29467c59f23f1420ddc145'
